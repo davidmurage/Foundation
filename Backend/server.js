d14c68d0ebee9c5fcd0194ee9e32897038797878
DavidMurage <davidmurage415@gmail.com>
2025-09-11 16:48:30 +0300

    updated the server

--- Backend/server.js
+++ Backend/server.js
@@ -21,6 +21,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 })
 .catch((err) => console.log(err));
 
+//rest Api
+app.get('/', (req, res) => {
+    res.send('API is running')
+});
 //Start Server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
