665db65bb4d467686e581c1e9a8c5fc832426ef9
DavidMurage <davidmurage415@gmail.com>
2026-02-02 15:26:37 +0300

    changed some styles

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -17,4 +17,8 @@ const userSchema = new mongoose.Schema({
   resetTokenExpiry: { type: Date }
 }, { timestamps: true });
 
+// INDEXES
+userSchema.index({ email: 1 }, { unique: true });
+userSchema.index({ role: 1 });
+
 export default mongoose.model("User", userSchema);
